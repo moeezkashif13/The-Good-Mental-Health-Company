a5736168f81c9ace9f78d8afc70a4c2ccf44146c
moeezkashif13 <moeezkashif13@gmail.com>
2023-08-19 14:18:41 +0500

    made improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,28 @@
 import FAQ from "@/Components/FAQ";
 import Footer from "@/Components/Footer";
-import HeroSect from "@/Components/HeroSect";
-import HowItWorks from "@/Components/How It Works";
-import Info from "@/Components/Info";
-import Location from "@/Components/Location";
+// import HeroSect from "@/Components/HeroSect";
+// import HowItWorks from "@/Components/How It Works";
+// import Info from "@/Components/Info";
+// import Location from "@/Components/Location";
 import Navbar from "@/Components/Navbar";
-import WhatWeTreat from "@/Components/What We Treat";
+// import WhatWeTreat from "@/Components/What We Treat";
 import Head from "next/head";
+
+import dynamic from 'next/dynamic'
+
+const HeroSect = dynamic(() => import('../Components/HeroSect'))
+
+
+const HowItWorks = dynamic(() => import('../Components/How It Works'))
+
+
+const Info = dynamic(() => import('../Components/Info'))
+
+const WhatWeTreat = dynamic(() => import('../Components/What We Treat'))
+
+const Location = dynamic(() => import('../Components/Location'))
+
+
 
 export default function Home() {
 
